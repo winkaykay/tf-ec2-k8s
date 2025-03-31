52911c71faa050c253607151c2a8351836aee6b2
winkaykay <winkaykaykhine@gmail.com>
2025-03-31 21:24:57 +0800

    changes in script 5

--- scripts/install_kubectl_jumphost.sh
+++ scripts/install_kubectl_jumphost.sh
@@ -2,14 +2,14 @@
 set -e
 
 RELEASE="$(curl -sSL https://dl.k8s.io/release/stable.txt)"
-RELEASE="${RELEASE%.*}"
+RELEASE="$${RELEASE%.*}"
 sudo bash -c "cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
-baseurl=https://pkgs.k8s.io/core:/stable:/${RELEASE}/rpm/
+baseurl=https://pkgs.k8s.io/core:/stable:/$${RELEASE}/rpm/
 enabled=1
 gpgcheck=1
-gpgkey=https://pkgs.k8s.io/core:/stable:/${RELEASE}/rpm/repodata/repomd.xml.key
+gpgkey=https://pkgs.k8s.io/core:/stable:/$${RELEASE}/rpm/repodata/repomd.xml.key
 EOF"
 sudo dnf install kubectl -y
 
